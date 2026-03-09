0457ecfee214be8a1253a9f2e0641e7607dadf81
Tamelc2 <Tamelcalloway42@gmail.com>
2026-03-09 14:19:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(“Website loaded”);
+import { createClient } from ‘https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm’
+
+const supabase = createClient(
+
+  https://wpunghbxjlqnbfnofumw.supabase.co
+)eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndwdW5naGJ4amxxbmJmbm9mdW13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NTA1NDQsImV4cCI6MjA4ODEyNjU0NH0.NpfzVyXekSgU8gdGX4BEhDPAm0u8W3QqWLKQybnEoXg
+
+async function loadJobs() {
+  const { data, error } = await supabase
+    .from(‘jobs’)
+    .select(‘*’)
+
+  console.log(data)
+}
+
+loadJobs()
